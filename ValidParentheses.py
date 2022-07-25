febabd25fa308af28bf69d3b997cfafce04b3387
Ridhma Agrawal <83450783+Ridhma25@users.noreply.github.com>
2022-07-25 20:05:32 +0530

    Valid Parentheses in Python Using Stack

---
+++ ValidParentheses.py
@@ -0,0 +1,16 @@
+def isValid(self, s: str) -> bool:
+        stack = []
+        
+        for i in s:
+            if stack:
+                if i==")" and stack[-1] == "(":
+                    stack.pop()
+                elif i=="}" and stack[-1] == "{":
+                    stack.pop()
+                elif i=="]" and stack[-1] == "[":
+                    stack.pop()
+                else:
+                    stack.append(i)
+            else:
+                stack.append(i)
+        return len(stack)==0
